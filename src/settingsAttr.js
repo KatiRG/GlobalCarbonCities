16694c4adafdc335cd16af2d4b37717e58fb3a76
katiRG <cnangini@gmail.com>
2019-07-14 13:47:06 -0400

    WCAG check for bar colours

--- src/settingsAttr.js
+++ src/settingsAttr.js
@@ -7,21 +7,23 @@ export default {
   },
   protocol: {
     whichLim: "d3extent",
-    colourRange: ["#9DD3DF", "#C3BBE2", "#E35B5D", "#EB9F9F", "#F18052", "#F4DD51"],
+    colourRange: ["#9DD3DF", "#8856a7", "#E35B5D", "#EB9F9F", "#F18052", "#F4DD51"],
     cbValues: [1, 2, 3, 4, 5, 6],
     xpos: [22, 86, 190, 273, 355, 417]
   },
   year: {
     whichLim: "d3extent",
     modx: 5,
-    colourRange: ["#D8E6DB", "#DBC28F", "red", "#997E68", "#6B5344"],
-    xpos: [15, 100, 184, 269, 355]
+    colourRange: ["#a63603", "#e6550d", "#997E68", "#fd8d3c", "#fecc5c"],
+    // colourRange: ["#fecc5c", "#fd8d3c", "#997E68", "#e6550d", "#a63603"],
+    xpos: [13, 98, 182, 267, 353]
   },
   population: {
     whichLim: "d3extent",
     // modx: 100000,
-    colourRange: ["#DED8B6", "#F9C869", "#5D5061", "#875979", "red"],
-    xpos: [49, 122, 203, 286, 365],
+    colourRange: ["#DED8B6", "#fbb4b9", "#f768a1", "#c51b8a", "#7a0177"],
+    // colourRange: ["#7a0177", "#c51b8a", "#f768a1", "#fbb4b9", "#DED8B6"],
+    xpos: [18, 97, 182, 271, 356],
     formatLevel: function(...args) {
       return d3.format(".2s")(args);
     }
@@ -30,7 +32,7 @@ export default {
     whichLim: "d3mean",
     modx: 1000,
     colourRange: ["#b8aca2", "#E394A7", "#9e9ac8", "#756bb1", "#54278f"],
-    xpos: [46, 127, 208, 288, 368],
+    xpos: [17, 105, 190, 275, 360],
     formatLevel: function(...args) {
       return d3.format(".2s")(args);
     }
@@ -38,27 +40,27 @@ export default {
   area: {
     whichLim: "d3mean",
     modx: 100,
-    colourRange: ["#EDDAD0", "#D5DED9", "#99B2B7", "#5b6a6d", "#948C75"],
-    xpos: [61, 127, 208, 288, 368]
+    colourRange: ["#ffffcc", "#c7e9b4", "#41b6c4", "#2c7fb8", "#253494"],
+    xpos: [32, 104, 189, 275, 361]
   },
   pop_density: {
     whichLim: "d3mean",
     modx: 100,
-    colourRange: ["#DED8B6", "#F9C869", "#E1F5C4", "#ADD6BC", "#486d6c"],
-    xpos: [48, 122, 202, 282, 355]
+    colourRange: ["#ffffcc", "#F9C869", "#E1F5C4", "#ADD6BC", "#486d6c"],
+    xpos: [20, 100, 183, 268, 346]
   },
   diesel: {
     whichLim: "d3extent",
-    colourRange: ["#F1F2C4", "#F2EA72", "#fec44f", "#c2cd7b", "#bf6456"],
-    xpos: [45, 125, 205, 285, 364],
+    colourRange: ["#ffffd4", "#fed98e", "#fe9929", "#d95f0e", "#993404"],
+    xpos: [17, 101, 187, 271, 356],
     formatLevel: function(...args) {
       return d3.format(".2f")(args);
     }
   },
   gas: {
     whichLim: "d3extent",
-    colourRange: ["#F1F2C4", "#F2EA72", "#fec44f", "#c2cd7b", "#bf6456"],
-    xpos: [45, 125, 205, 285, 364],
+    colourRange: ["#F1F2C4", "#fecc5c", "#fd8d3c", "#f03b20", "#bd0026"],
+    xpos: [16, 102, 185, 271, 357],
     formatLevel: function(...args) {
       return d3.format(".2f")(args);
     }
@@ -66,19 +68,19 @@ export default {
   HDD: {
     whichLim: "d3extent",
     modx: 1000,
-    colourRange: ["#F5F5C6", "#F5DDB5", "#d6c2d0", "#a27696", "#b8d7ff"],
-    xpos: [61, 122, 201, 282, 362]
+    colourRange: ["#feebe2", "#fbb4b9", "#f768a1", "#c51b8a", "#7a0177"],
+    xpos: [32, 97, 182, 268, 352]
   },
   CDD: {
     whichLim: "d3extent",
     modx: 1000,
-    colourRange: ["#E1F5C4", "#ffeda0", "#F9D423", "#FC913A", "#FF4E50"],
-    xpos: [61, 122, 201, 282, 362]
+    colourRange: ["#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"],
+    xpos: [32, 104, 189, 268, 352]
   },
   low_bua_pc_2014: {
     whichLim: "d3extent",
     colourRange: ["#ECDAA8", "#B6AC7B", "#8C9C82", "#9AA0AC", "#70725A"],
-    xpos: [46, 124, 205, 285, 366],
+    xpos: [16, 101, 185, 270, 355],
     formatLevel: function(...args) {
       return d3.format(".2f")(args);
     }
@@ -86,7 +88,7 @@ export default {
   high_bua_pc_2014: {
     whichLim: "d3extent",
     colourRange: ["#ECDAA8", "#B6AC7B", "#8C9C82", "#9AA0AC", "#70725A"],
-    xpos: [46, 124, 205, 285, 366],
+    xpos: [16, 101, 185, 270, 355],
     formatLevel: function(...args) {
       return d3.format(".2f")(args);
     }
