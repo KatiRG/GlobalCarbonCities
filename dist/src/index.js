009d7be0e8f014569607b0b6c8e447bb664c5a90
Laurent Goderre <laurent.goderre@gmail.com>
2019-07-19 23:44:08 -0400

    Fixed the build not copying all the assets

--- dist/src/index.js
+++ dist/src/index.js
@@ -6,7 +6,7 @@
 	}
 
 	var _core = createCommonjsModule(function (module) {
-	var core = module.exports = { version: '2.6.5' };
+	var core = module.exports = { version: '2.6.9' };
 	if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
 	});
 	var _core_1 = _core.version;
@@ -30,7 +30,7 @@
 	  return store[key] || (store[key] = value !== undefined ? value : {});
 	})('versions', []).push({
 	  version: _core.version,
-	  mode: 'global',
+	  mode: _library ? 'pure' : 'global',
 	  copyright: '© 2019 Denis Pushkarev (zloirock.ru)'
 	});
 	});
